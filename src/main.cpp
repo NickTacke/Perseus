e7e0a9c6e87913a6c9bcb38308d93b15800b049a
NickTacke <the.nick.tacke@gmail.com>
2023-12-11 21:13:14 +0100

    Start/stop webserver

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,19 @@
 #include <chrono>
 #include <fstream>
 
+#include "Web/WebServer.h"
+
 #include "Utils/Memory/Memory.h"
 
 int main() {
+    int port = 8080; // Choose your desired port
+
+    WebServer server(port);
+
+    std::thread serverThread([&server]() {
+        server.start();
+    });
+
     // Wait for the R5Apex.exe process
     pid_t processId = Memory::GetProcessId("r5apex.exe");
     std::cout << "Found r5apex.exe with process id: " << processId << std::endl;
@@ -16,11 +26,15 @@ int main() {
 
     while(true) {
         // moduleBase + localPlayer + m_iHealth
-        std::cout << Memory::ReadString(moduleBase + 0x16966f0) << std::endl;
+        //std::cout << Memory::Read<int>(Memory::Read<unsigned long>(moduleBase + 0x211fac8) + 0x036c) << std::endl;
 
         // Wait a millisecond to prevent freezing
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
+
+    server.stop();
+
+    serverThread.join();
 
     return -1;
 }
